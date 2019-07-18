7d7c4010899e581f97fc684a4d68fc775073d0d6
fandradetecinfo <fandradetecinfo@gmail.com>
2019-07-18 12:37:29 -0300

    Evolve

--- Code/TicketManager.MVC/Global.asax.cs
+++ Code/TicketManager.MVC/Global.asax.cs
@@ -1,6 +1,11 @@
 ﻿using System.Web.Mvc;
 using System.Web.Routing;
 using System.Globalization;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Web;
 
 namespace TicketManager.MVC
 {
@@ -12,6 +17,43 @@ namespace TicketManager.MVC
             RouteConfig.RegisterRoutes(RouteTable.Routes);
 
             System.Threading.Thread.CurrentThread.CurrentCulture = new CultureInfo("pt-BR");
+
+//            var strconn = System.Configuration.ConfigurationManager.ConnectionStrings["TicketDBEntities"].ConnectionString;
+
+            try
+            {
+                //var x = HttpContext.Current.Server..Request.AppRelativeCurrentExecutionFilePath.Replace("~/", "");
+
+                var path = Server.MapPath("/");
+
+                //var evolveConnection = new MySql.Data.MySqlClient.MySqlConnection(strconn);
+                //var evolve = new Evolve.Evolve("evolve.json", evolveConnection, msg => _logger.LogInformation(msg))
+                var evolve = new Evolve.Evolve(path + "/App_Data/Evolve.json")
+                {
+                    Locations = new List<string> { path + "/db/migrations" },
+                    IsEraseDisabled = true,
+                };
+
+                evolve.Migrate();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Database migration failed.", ex);
+                //_logger.LogCritical("Database migration failed.", ex);
+                throw;
+            }
+
+
+        }
+
+        private void DoMigrations()
+        {
+
+            using (StreamReader r = new StreamReader("Evolve.json"))
+            {
+                string json = r.ReadToEnd();
+                //RootObject company = JsonConvert.DeserializeObject<RootObject>(json);
+            }
         }
     }
 }
---
+++ Code/TicketManager.MVC/db/migrations/V2_0_1__Create_Table_Customer.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE `tm_customer` (
+  `CustomerId` int(11) NOT NULL AUTO_INCREMENT,
+  `CustomerName` varchar(80) NOT NULL,
+  `CustomerAbrev` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`CustomerId`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
